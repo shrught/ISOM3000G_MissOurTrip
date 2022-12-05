396969fe65a0b6a4ebf1485ac39731e976376aa9
shrught <51689102+shrught@users.noreply.github.com>
2022-12-05 14:34:09 +0800

    Update password activities

--- app/src/main/java/com/example/missourtrip/ResetPasswordActivity.kt
+++ app/src/main/java/com/example/missourtrip/ResetPasswordActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_login.*
 import android.content.Intent
+import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_reset_password.*
 import java.io.File
@@ -20,19 +21,26 @@ class ResetPasswordActivity : AppCompatActivity() {
         val saved_password = File(path, "password.txt")
 
         if(current_password.text.toString() == saved_password.readText()){
-            saved_password.writeText(new_password.text.toString());
-            val reset_successful = Snackbar.make(view, "Password Reset Successful, Please Login again!!", Snackbar.LENGTH_SHORT)
-            reset_successful.show();
 
-            val to_login_intent = Intent(this, LoginActivity::class.java).apply {
+            if(new_password.text == null){
+                Toast.makeText(this, "Password Can't be Null!!",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                saved_password.writeText(new_password.text.toString());
+                val reset_successful = Snackbar.make(view, "Password Reset Successful, Please Login again!!", Snackbar.LENGTH_SHORT)
+                reset_successful.show();
+
+                val to_login_intent = Intent(this, LoginActivity::class.java).apply {
+
+                }
+                startActivity(to_login_intent)
 
             }
-            startActivity(to_login_intent)
+
 
         }
         else{
-            val warning = Snackbar.make(view, "Current Password is Wrong!!", Snackbar.LENGTH_SHORT)
-            warning.show();
+            Toast.makeText(this, "Current Password is Wrong!!",Toast.LENGTH_SHORT).show()
         }
 
     }
