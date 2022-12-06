f45a95ed0354dacc76537f3f191ae46c0479a7c6
Sarah Leung <60539445+sarah-leung@users.noreply.github.com>
2022-12-06 13:45:16 +0800

    Refactored StatisticsActivity to use StatisticsAdapter

--- app/src/main/java/com/example/missourtrip/StatisticsActivity.kt
+++ app/src/main/java/com/example/missourtrip/StatisticsActivity.kt
@@ -7,8 +7,12 @@ import android.widget.LinearLayout
 import android.widget.Space
 import android.widget.TableRow
 import android.widget.TextView
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.room.Room
+import kotlinx.android.synthetic.main.activity_expense_main.*
 import kotlinx.android.synthetic.main.activity_statistics.*
+import kotlinx.android.synthetic.main.activity_statistics.recycleView_exp
 
 data class Summary (val category: String, var currencies: ArrayList<CurrencySummary> = ArrayList())
 data class CurrencySummary (val currency: String, var total: Double = 0.0)
@@ -17,26 +21,36 @@ class StatisticsActivity : AppCompatActivity() {
     private lateinit var categoryDB: AppDatabase
     private lateinit var expenseDB: AppDatabase
     private lateinit var summary:ArrayList<Summary>
+    private lateinit var statisticsAdapter: StatisticsAdapter
+    private lateinit var linearLayoutManager_exp: LinearLayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_statistics)
-
+        
         categoryDB = Room.databaseBuilder(this,
             AppDatabase::class.java,
             "categories").allowMainThreadQueries().build()
-
+        
         expenseDB = Room.databaseBuilder(this,
             AppDatabase::class.java,
             "expenses").allowMainThreadQueries().build()
+        
+        summary = arrayListOf()
+        linearLayoutManager_exp = LinearLayoutManager(this)
+        
+        statisticsAdapter = StatisticsAdapter(summary)
+        recycleView_exp.apply {
+            adapter = statisticsAdapter
+            layoutManager = linearLayoutManager_exp
+        }
+        recycleView_exp.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
 
         calculateStatistics()
         displayStatistics()
     }
 
     private fun calculateStatistics() {
-        summary = ArrayList()
-
         for (category in categoryDB.categoryDao().getName()) {
             summary.add(Summary(category))
         }
@@ -58,52 +72,12 @@ class StatisticsActivity : AppCompatActivity() {
                 }
             }
         }
+        summary.sortByDescending{ it.currencies.size }
         Log.d("summary", summary.joinToString())
     }
 
     private fun displayStatistics() {
-        summary_table.removeAllViews()
-
-        val newRows = ArrayList<TableRow>()
-        val newSpaces = ArrayList<Space>()
-        val newCategoryNames = ArrayList<TextView>()
-        val newLinearLayout = ArrayList<LinearLayout>()
-        val newCurrencyTotals = ArrayList<ArrayList<TextView>>()
-
-        for ((row, value) in summary.withIndex()) {
-            newCategoryNames.add(TextView(this))
-            newCategoryNames[row].text = value.category
-            newCategoryNames[row].width = 400
-
-            // list of currency totals
-            newLinearLayout.add(LinearLayout(this))
-            newLinearLayout[row].orientation = LinearLayout.VERTICAL
-            newCurrencyTotals.add(ArrayList<TextView>())
-            for ((col, value) in summary[row].currencies.withIndex()) {
-                newCurrencyTotals[row].add(TextView(this))
-                newCurrencyTotals[row][col].text = "%s %.2f".format(value.currency, value.total)
-                newCurrencyTotals[row][col].maxWidth = 400
-                newLinearLayout[row].addView(newCurrencyTotals[row][col])
-                Log.d("currencies summary", "$col, $value")
-            }
-
-            if (summary[row].currencies.size == 0) {
-                newCurrencyTotals[row].add(TextView(this))
-                newCurrencyTotals[row][0].text = "-"
-                newCurrencyTotals[row][0].maxWidth = 400
-                newLinearLayout[row].addView(newCurrencyTotals[row][0])
-            }
-
-            newSpaces.add(Space(this))
-            newSpaces[row].minimumWidth = 100
-
-            newRows.add(TableRow(this))
-            newRows[row].addView(newCategoryNames[row])
-            newRows[row].addView(newSpaces[row])
-            newRows[row].addView(newLinearLayout[row])
-            summary_table.addView(newRows[row])
-        }
-
+        statisticsAdapter.setData(summary)
         var summaryCurrencies = summary.map { it.currencies }.flatten().groupBy {it.currency }
             .map { (key,value) ->
                 var total = 0.0
